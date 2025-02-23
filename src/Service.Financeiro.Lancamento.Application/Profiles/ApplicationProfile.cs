5999f6aa030c29aebaf1977975b9d193d0ee609f
Luis Fernando Gouvea <lgouvea@actioncargo.com.br>
2025-02-23 12:58:21 -0300

    - configura RMQ - Adiciona EF inMemory

--- src/Service.Financeiro.Lancamento.Application/Profiles/ApplicationProfile.cs
+++ src/Service.Financeiro.Lancamento.Application/Profiles/ApplicationProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Service.Financeiro.Lancamento.Application.Applications.v1.AdicionarLancamento;
+using Service.Financeiro.Lancamento.Infrastructure.Events;
 using LancamentoDb = Service.Financeiro.Lancamento.Domain.Entities.Lancamento;
 
 namespace Service.Financeiro.Lancamento.Application.Profiles
@@ -8,6 +9,7 @@ namespace Service.Financeiro.Lancamento.Application.Profiles
     {
         public ApplicationProfile()
         {
+            CreateMap<LancamentoDb, ConsolidarLancamentoEvent>().ReverseMap();
             CreateMap<LancamentoDb, AdicionarLancamentoCommand>().ReverseMap();
             CreateMap<LancamentoDb, AdicionarLancamentoCommandResponse>().ReverseMap();
         }
--- src/Service.Financeiro.Lancamento.Presentation/Program.cs
+++ src/Service.Financeiro.Lancamento.Presentation/Program.cs
@@ -1,10 +1,27 @@
+using AutoMapper;
+using ClosedXML.Parser;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
 using Service.Financeiro.Lancamento.Application.Applications.v1.AdicionarLancamento;
+using Service.Financeiro.Lancamento.Application.Profiles;
+using Service.Financeiro.Lancamento.Persistence.Context;
+using Service.Financeiro.Lancamento.Persistence.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
+builder.Services.AddDbContext<LancamentoContext>(options => options.UseInMemoryDatabase("TestDatabase"));
 builder.Services.AddControllers();
+builder.Services.AddScoped<ILancamentoRepository, LancamentoRepository>();
+builder.Services.AddSingleton<IMapper>(sp =>
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile(new ApplicationProfile());
+    });
+    return config.CreateMapper();
+});
+
 builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssembly(typeof(AdicionarLancamentoCommandHandler).Assembly);
@@ -19,15 +36,18 @@ builder.Services.AddMassTransit(bus =>
 
 });
 
-builder.Services.AddMassTransitHostedService();
-
-
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
